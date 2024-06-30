bc19c120855c9f3b676084d7b3544211b708ed2c
NicolasKowal <nicolas.kowal95@gmail.com>
2024-06-30 12:24:51 -0300

    nav

--- el-refugio-austral/src/navbar/Navbar.jsx
+++ el-refugio-austral/src/navbar/Navbar.jsx
@@ -3,67 +3,92 @@ import "./Navbar.scss";
 
 function Navbar() {
 	return (
-		<ul className="BarraDeNavegacion">
-			<li>
-				<a href="">
-					<img src="./img/tienda-de-animales.png" alt="imgref" />
-				</a>
-			</li>
-			<li>
-				<a href="">Inicio</a>
-			</li>
-			<li className="nav-item dropdown">
-				<a
-					class="nav-link dropdown-toggle"
-					href="#"
-					role="button"
-					data-bs-toggle="dropdown"
-					aria-expanded="false"
-				>
-					Productos
-				</a>
-				<ul class="dropdown-menu">
-					<li>
-						<a className="dropdown-item" href="#">
-							Alimentos
-						</a>
-					</li>
-					<li>
-						<a className="dropdown-item" href="#">
-							Ropa
-						</a>
-					</li>
-					<li>
-						<a className="dropdown-item" href="#">
-							Juguetes
-						</a>
-					</li>
-					<li>
-						<a className="dropdown-item" href="#">
-							Veterinaria
-						</a>
-					</li>
-					<li>
-						<a className="dropdown-item" href="#">
-							Collares
-						</a>
-					</li>
-				</ul>
-			</li>
-			<li>
-				<a href="">Contacto</a>
-			</li>
-			<li>
-				<div className="carritoDeCompras">
-					<a href="">
-						<div>
-							<img src="./img/carrito-de-compras.png" alt="imgref" />
-						</div>
-						<p>-</p>
+		<>
+			<nav className=" navbar navbar-expand-lg bg-body-tertiary">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						<img src="./img/tienda-de-animales.png" alt="imgref" />
 					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul className="BarraDeNavegacion navbar-nav">
+							<li className="nav-item">
+								<a
+									className="nav-link active text-center"
+									aria-current="page"
+									href="#"
+								>
+									¿Como comprar?
+								</a>
+							</li>
+							<li className="nav-item dropdown">
+								<a
+									className="nav-link dropdown-toggle"
+									href="#"
+									role="button"
+									data-bs-toggle="dropdown"
+									aria-expanded="false"
+								>
+									Productos
+								</a>
+								<ul className="dropdown-menu">
+									<li>
+										<a className="dropdown-item" href="#">
+											Alimentos
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Ropa
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Juguetes
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Accesorios
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Veterinaria
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Contacto
+								</a>
+							</li>
+							<li>
+								<div className="carritoDeCompras">
+									<a href="">
+										<div>
+											<img src="./img/carrito-de-compras.png" alt="imgref" />
+										</div>
+										<p></p>
+									</a>
+								</div>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</li>
-		</ul>
+			</nav>
+		</>
 	);
 }
 
