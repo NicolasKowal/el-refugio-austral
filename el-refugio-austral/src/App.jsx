2fe4cc381ac6cb2137be9ea15b630b76d212c98b
NicolasKowal <nicolas.kowal95@gmail.com>
2024-06-29 21:54:30 -0300

    gg

--- el-refugio-austral/src/App.jsx
+++ el-refugio-austral/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./navbar/Navbar";
+import Landing from "./landing/Landing";
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Landing />
 		</>
 	);
 }
